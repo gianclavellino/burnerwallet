8be486fccf85c961975624433a566fbc3089b10a
Gian Clavellino <info@clavellino.it>
2025-01-04 21:16:16 -0300

    Update App.jsx

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -945,7 +945,7 @@ function App(props) {
         
       )} 
       <br />
-      <h1 id="nftickets" style={{ textAlign: "center" }}>NFTickets</h1>
+      <h1 id="nftickets" style={{ textAlign: "center" color: "#FFFFFF" }}>NFTickets</h1>
 
     
 
